f8877ba613b6a6dc04fab3a2d9243a047f2c3434
pallavijawanjal <pallavij@gmail.com>
2022-08-08 19:46:21 +0530

    Add project files.

---
+++ EmploeeWageProblme/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmploeeWageProblme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UC_First_EmployeeAttendance.Emp_Attendance();
+            UC_Second_DailyEmpWage.EmployeeWage();
+            UC3_PartTimeWage.PartTimeEmpWage();
+            UC4_EmpWageUsingSwitch.EmpWageUsingSwitch();
+            UC5_EmpWageForMonth.CalculateWagesFor20DaysInMonth();
+            UC6_EmpWageTillHrOrReached.CalculateWageTil_HrsOr_IsReached();
+        }
+    }
+}
